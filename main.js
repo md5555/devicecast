af1f7d280ef4d02693b6b0baab0c8ca90268fce4
Milosz Derezynski <milosz@Miloszs-MacBook-Pro.local>
2017-05-03 21:28:55 +0200

    Cleanup / Fix connect -> sleep -> reconnect cycle

--- main.js
+++ main.js
@@ -15,19 +15,17 @@ var mb = menubar({dir: __dirname, icon: 'not-castingTemplate.png'});
 /* Internals */
 var MenuFactory = require('./lib/native/MenuFactory');
 var NotificationService = require('./lib/native/NotificationService');
-
 var DeviceLookupService = require('./lib/device/utils/DeviceLookupService');
 var DeviceMatcher = require('./lib/device/utils/DeviceMatcher');
 var LocalSourceSwitcher = require('./lib/device/utils/LocalSourceSwitcher');
 var UpnpMediaClientUtils = require('./lib/device/utils/UpnpMediaClientUtils');
-
 var LocalSoundStreamer = require('./lib/sound/LocalSoundStreamerWebcast');
 
 /* Various Device controllers */
-var RaumfeldZone = require('./lib/device/controls/RaumfeldZone');
-var ChromeCast = require('./lib/device/controls/ChromeCast');
-var logger = require('./lib/common/logger');
-const osxsleep = require ('osxsleep');
+const RaumfeldZone = require('./lib/device/controls/RaumfeldZone');
+const ChromeCast = require('./lib/device/controls/ChromeCast');
+const logger = require('./lib/common/logger');
+const osxsleep = require('osxsleep');
 
 var currentDevice = null;
 var menu = null;
@@ -36,234 +34,246 @@ var streamingAddress;
 var reconnect = false;
 var reconnectName = null;
 var switchingDevice = false;
-
 var deviceMenuChromecast = null;
 var deviceMenuUPnP = null;
 var deviceListMenu = null;
 
-var fullReset = function() {
+var fullReset = function () {
 
-	stopCurrentDevice();
+    stopCurrentDevice();
 
-	if (currentDevice != null) {
-	    currentDevice.doConnect();
-	}
-};
-
-var stopDevice = function (callback, device) {
-
-	if (device) {
-
-	    logger.info("Stopping CURRENT device %s", currentDevice.name);
-
-	    device.controls.stop(function(){
-
-		    // Show user notification
-		    NotificationService.notifyCastingStopped(device.controls);
-
-		    var menus = [ deviceListMenu, deviceMenuChromecast, deviceMenuUPnP ];
-
-		    // set cast icon when playing
-
-		    for (var a = 0; a < menus.length; a++) {
-
-			    var opMenu = menus[a];
-
-			    for (var n = 0; n < opMenu.items.length; n++) {
-				opMenu.setIcon(n, null);
-			    }
-		    }
-
-		    // Switch tray icon
-		    mb.tray.setImage(path.join(__dirname, 'not-castingTemplate.png'));
-
-		    if (callback) callback();
-	    });
-	} else {
-	    if (callback) callback();
-	}
-    };
-
-var stopCurrentDevice = function(callback) {
-    stopDevice(callback, currentDevice);	
-};
-
-var stopCurrentDeviceMatch = function(callback, matchDevice) {
-    
-    if (matchDevice != currentDevice) {
-	stopCurrentDevice(callback);
-    } else {
-	callback();
+    if (currentDevice !== null) {
+        currentDevice.doConnect();
     }
 };
 
-var onStartStream = function(cb) {
+var clearIcons = function () {
+
+    var menus = [deviceListMenu, deviceMenuChromecast, deviceMenuUPnP];
+
+    for (var a = 0; a < menus.length; a++) {
+
+        var opMenu = menus[a];
+
+        for (var n = 0; n < opMenu.items.length; n++) {
+            opMenu.setIcon(n, null);
+        }
+    }
+}
+
+function setTrayIconNotCasting() {
+    mb.tray.setImage(path.join(__dirname, 'not-castingTemplate.png'));
+}
+var stopDevice = function (callback, device) {
+
+    if (device) {
+
+        logger.info("Stopping CURRENT device %s", currentDevice.name);
+
+        device.controls.stop(function () {
+
+            clearIcons();
+            setTrayIconNotCasting();
+
+            if (callback) callback();
+        });
+    } else {
+        if (callback) callback();
+    }
+};
+
+var stopCurrentDevice = function (callback) {
+    stopDevice(callback, currentDevice);
+};
+
+var stopCurrentDeviceMatch = function (callback, matchDevice) {
+
+    if (matchDevice !== currentDevice) {
+        stopCurrentDevice(callback);
+    } else {
+        callback();
+    }
+};
+
+var onStartStream = function (cb) {
 
     LocalSoundStreamer.startStream(function (streamUrl) {
         streamingAddress = streamUrl;
-	if (cb) cb();
-    }, function(err){
-    }, function() {
-	onStop();
+        if (cb) cb();
+    }, function (err) {
+    }, function () {
+        onStop();
     });
 };
 
-var onStop = function() {
+var onStop = function () {
 
     if (switchingDevice) {
         return;
     }
-   
-    onStartStream(function(){
-	if (currentDevice) {
-	    currentDevice.doConnect();
-	}
-    });
+
+    NotificationService.notifyCastingStopped(device.controls);
 };
 
 var onStreamingUpdateUI = function () {
 
-	switchingDevice = false;
+    switchingDevice = false;
 
-	deviceListMenu.setIcon(0, null);
-	deviceListMenu.setIcon(1, null);
+    deviceListMenu.setIcon(0, null);
+    deviceListMenu.setIcon(1, null);
 
-	for (var n = 0; n < this.opMenu.items.length; n++) {
-		if (this.opMenu.items[n].label === this.device.name) {
-			logger.info("Setting icon!");
-			var castIcon = nativeImage.createFromPath(path.join(__dirname, 'castingTemplate-small.png'));
-			this.opMenu.setIcon(n, castIcon);
-			deviceListMenu.setIcon(this.a, castIcon);
-		}
-	}
+    for (var n = 0; n < this.opMenu.items.length; n++) {
+        if (this.opMenu.items[n].label === this.device.name) {
+            logger.info("Setting icon!");
+            var castIcon = nativeImage.createFromPath(path.join(__dirname, 'castingTemplate-small.png'));
+            this.opMenu.setIcon(n, castIcon);
+            deviceListMenu.setIcon(this.a, castIcon);
+        }
+    }
 
-        mb.tray.setImage(path.join(__dirname, 'castingTemplate.png'));
-    	mb.tray.setContextMenu(menu);
-    };
+    mb.tray.setImage(path.join(__dirname, 'castingTemplate.png'));
+    mb.tray.setContextMenu(menu);
+};
 
-var scanForDevices = function(self) {
+var scanForDevices = function () {
 
     DeviceLookupService.lookUpDevices(function onDevice(device) {
 
-	var found = false;
+        var found = false;
 
-	for (var n = 0; n < devicesAdded.length; n++) {
+        for (var n = 0; n < devicesAdded.length; n++) {
 
-		var n0 = device.name+":"+device.type;
-		var n1 = devicesAdded[n].name+":"+devicesAdded[n].type;
+            var n0 = device.name + ":" + device.type;
+            var n1 = devicesAdded[n].name + ":" + devicesAdded[n].type;
 
-		if(n0 == n1) {
-	            found = true;
-		    break; 
-		}
-	}
+            if (n0 === n1) {
+                found = true;
+                break;
+            }
+        }
 
-	if (!found) {
-	   devicesAdded.push(device);
-	} else {
-	   return;
+        if (!found) {
+            devicesAdded.push(device);
+        } else {
+            return;
         }
 
         switch (device.type) {
             case DeviceMatcher.TYPES.CHROMECAST:
 
-		var doConnectCast = function onClicked() {
+                var doConnectCast = function onClicked() {
 
-		    device.controls = new ChromeCast(device);
+                    if (switchingDevice) {
+                        return;
+                    }
 
-		    switchingDevice = true;
-		    LocalSoundStreamer.stopStream();
+                    switchingDevice = true;
 
-		    logger.info('Attempting to play to Chromecast', device.name);
+                    device.controls = new ChromeCast(device);
 
-		    stopCurrentDeviceMatch(function() {
-	
-			    // Sets OSX selected input and output audio devices to Soundflower
-			    LocalSourceSwitcher.switchSource({
-				output: 'Soundflower (2ch)',
-				input: 'Soundflower (2ch)'
-			    });
+                    LocalSoundStreamer.stopStream();
 
-			    storage.set('reconnect', { 
-					    setting: reconnect,
-					    name: device.name
-				    }, function(error){
-					    if (error == null) {
-						    return;
-					    }
-					    logger.info("error while storing setting: %s", error.toString());
-				    });
+                    logger.info('Attempting to play to Google Cast device: ', device.name);
 
-			    currentDevice = device;
+                    stopCurrentDeviceMatch(function () {
 
-			    onStartStream(function() {
-			        device.controls.play(streamingAddress, onStreamingUpdateUI.bind({device: device, a: 0, opMenu: deviceMenuChromecast}));
-			    });
+                        // Sets OSX selected input and output audio devices to Soundflower
+                        LocalSourceSwitcher.switchSource({
+                            output: 'Soundflower (2ch)',
+                            input: 'Soundflower (2ch)'
+                        });
 
-		    }, device);
+                        storage.set('reconnect', {
+                            setting: reconnect,
+                            name: device.name
+                        }, function (error) {
+                            if (error === null) {
+                                return;
+                            }
+                            logger.info("error while storing setting: %s", error.toString());
+                        });
 
-		};
+                        currentDevice = device;
 
-		device.doConnect = doConnectCast;
+                        onStartStream(function () {
+                            device.controls.play(streamingAddress, onStreamingUpdateUI.bind({
+                                device: device,
+                                a: 0,
+                                opMenu: deviceMenuChromecast
+                            }));
+                        });
 
-		deviceMenuChromecast.append(MenuFactory.chromeCastItem(device, doConnectCast));
-		
+                    }, device);
+
+                };
+
+                device.doConnect = doConnectCast;
+
+                deviceMenuChromecast.append(MenuFactory.chromeCastItem(device, doConnectCast));
+
                 break;
 
             case DeviceMatcher.TYPES.UPNP:
 
                 if (DeviceMatcher.isRaumfeld(device)) {
 
-		    var doConnectUPnP = function onClicked() {
+                    var doConnectUPnP = function onClicked() {
+
+                        if (switchingDevice) {
+                            return;
+                        }
+
+                        switchingDevice = true;
 
                         device.controls = new RaumfeldZone(device);
 
-			switchingDevice = true;
-			LocalSoundStreamer.stopStream();
+                        LocalSoundStreamer.stopStream();
 
-                        logger.info('Attempting to play to Raumfeld device', device.name);
+                        logger.info('Attempting to play on Raumfeld Zone: ', device.name);
 
-		    	stopCurrentDeviceMatch(function() {
-		
-				// Sets OSX selected input and output audio devices to Soundflower
-				LocalSourceSwitcher.switchSource({
-				    output: 'Soundflower (2ch)',
-				    input: 'Soundflower (2ch)'
-				});
+                        stopCurrentDeviceMatch(function () {
 
-				currentDevice = device;
+                            // Sets OSX selected input and output audio devices to Soundflower
+                            LocalSourceSwitcher.switchSource({
+                                output: 'Soundflower (2ch)',
+                                input: 'Soundflower (2ch)'
+                            });
 
-				storage.set('reconnect', { 
-						setting: reconnect,
-						name: device.name
-					}, function(error){
-						if (error == null) {
-							return;
-						}
-						logger.info("error while storing setting: %s", error.toString());
-					});
+                            currentDevice = device;
 
-				device.controls.registerErrorHandler(function(err){
-				   onStop();
-				});
+                            storage.set('reconnect', {
+                                setting: reconnect,
+                                name: device.name
+                            }, function (error) {
+                                if (error === null) {
+                                    return;
+                                }
+                                logger.info("error while storing setting: %s", error.toString());
+                            });
 
-				onStartStream(function() {
-				    device.controls.play(streamingAddress, onStreamingUpdateUI.bind({device: device, a: 1, opMenu: deviceMenuUPnP}));
-				});
+                            device.controls.registerErrorHandler(function () {
+                                onStop();
+                            });
 
-			}, device);
+                            onStartStream(function () {
+                                device.controls.play(streamingAddress, onStreamingUpdateUI.bind({
+                                    device: device,
+                                    a: 1,
+                                    opMenu: deviceMenuUPnP
+                                }));
+                            });
+
+                        }, device);
 
                     };
 
-		    device.doConnect = doConnectUPnP;
-		
+                    device.doConnect = doConnectUPnP;
+
                     deviceMenuUPnP.append(MenuFactory.raumfeldDeviceItem(device, doConnectUPnP));
 
-		    logger.info('Added Raumfeld menu item (reconnect name: %s)', reconnectName);
-
-		    if (reconnectName != null && (reconnectName.localeCompare(device.name) == 0)) {
-			doConnectUPnP();
-		    }
+                    if (reconnectName !== null && (reconnectName.localeCompare(device.name) === 0)) {
+                        doConnectUPnP();
+                    }
                 }
                 break;
             default:
@@ -271,47 +281,49 @@ var scanForDevices = function(self) {
         }
 
         mb.tray.setContextMenu(menu);
-   });
+    });
 
 };
 
 //Menubar construction
 mb.on('ready', function ready() {
 
-    osxsleep.OSXSleep.start(function(state) {
-	logger.info("sleep state: %d", state);
+    osxsleep.OSXSleep.start(function (state) {
+        logger.info("sleep state: %d", state);
 
-	switch (state) {
-		case osxsleep.HAS_POWERED_ON:
-			if (currentDevice) {
-			    currentDevice.doConnect();
-			}
-			break;
-		case osxsleep.WILL_SLEEP:
-
-			switchingDevice = true;
-			stopCurrentDevice();
-
-			break;
-	}
+        switch (state) {
+            case osxsleep.HAS_POWERED_ON:
+                if (currentDevice) {
+                    switchingDevice = false;
+                    onStartStream(function () {
+                        currentDevice.doConnect();
+                    })
+                }
+                break;
+            case osxsleep.WILL_SLEEP:
+                stopCurrentDevice(function () {
+                    LocalSoundStreamer.stopStream();
+                });
+                break;
+        }
     });
-  
+
     storage.get('reconnect',
-		function(error, object){
+        function (error, object) {
 
-			if (object == null) {
-		    		return;
-			}
+            if (object === null) {
+                return;
+            }
 
-			reconnect = object.setting;
-			reconnectName = object.name;
+            reconnect = object.setting;
+            reconnectName = object.name;
 
-			logger.info("reconnect: "+reconnect+" name: "+reconnectName);
-	    	});
+            logger.info("reconnect: " + reconnect + " name: " + reconnectName);
+        });
 
-    process.on('uncaughtException', function(err) {
-	fullReset();
-	dialog.showErrorBox("devicecast - An Error Occurred", err.toString());
+    process.on('uncaughtException', function (err) {
+        fullReset();
+        dialog.showErrorBox("devicecast - An Error Occurred", err.toString());
     });
 
     //Menu startup message
@@ -327,24 +339,24 @@ mb.on('ready', function ready() {
     menu.append(MenuFactory.castToDeviceMenu(deviceListMenu));
 
     /*
-    menu.append(new MenuItem({
-        label: 'Reconnect on Start',
-	type: 'checkbox',
-        click: function (item, window, event) {
-	    reconnect = item.checked;
-	    reconnectName = self.currentDevice.name;
-	    storage.set('reconnect', { 
-		setting: reconnect,
-		name: self.currentDevice.name
-		 }, function(error){
-			if (error == null) {
-				return;
-			}
-			logger.info("error while storing setting: %s", error.toString());
-		});
-        }
-    }));
-    */
+     menu.append(new MenuItem({
+     label: 'Reconnect on Start',
+     type: 'checkbox',
+     click: function (item, window, event) {
+     reconnect = item.checked;
+     reconnectName = self.currentDevice.name;
+     storage.set('reconnect', {
+     setting: reconnect,
+     name: self.currentDevice.name
+     }, function(error){
+     if (error == null) {
+     return;
+     }
+     logger.info("error while storing setting: %s", error.toString());
+     });
+     }
+     }));
+     */
 
     menu.append(MenuFactory.separator());
 
@@ -352,9 +364,9 @@ mb.on('ready', function ready() {
         label: 'Volume Up',
         click: function () {
 
-		if (!currentDevice) return;
+            if (!currentDevice) return;
 
-		currentDevice.controls.volumeUp();
+            currentDevice.controls.volumeUp();
         }
     }));
 
@@ -362,12 +374,12 @@ mb.on('ready', function ready() {
         label: 'Volume Down',
         click: function () {
 
-		if (!currentDevice) return;
-	
-		currentDevice.controls.volumeDown();
+            if (!currentDevice) return;
+
+            currentDevice.controls.volumeDown();
         }
     }));
- 
+
     // Stream Options
     var streamMenu = new Menu();
     streamMenu.append(new MenuItem({
@@ -410,14 +422,13 @@ mb.on('ready', function ready() {
         enabled: true,
         click: function () {
             stopCurrentDevice();
-	    currentDevice = null;
+            currentDevice = null;
         }
     }));
 
     var onQuitHandler = function () {
-	switchingDevice = true;
-	osxsleep.OSXSleep.stop();
-        mb.tray.setImage(path.join(__dirname, 'not-castingTemplate.png'));
+        switchingDevice = false;
+        osxsleep.OSXSleep.stop();
         stopCurrentDevice();
         LocalSoundStreamer.stopStream();
         LocalSourceSwitcher.resetOriginSource();
@@ -432,12 +443,14 @@ mb.on('ready', function ready() {
 
     // Clicking this option starts casting audio to Cast
     menu.append(MenuFactory.separator());
- 
+
     // CMD + C death
     mb.app.on('quit', onQuitHandler);
 
     // Set the menu items
     mb.tray.setContextMenu(menu);
 
-    onStartStream(() => scanForDevices());
+    onStartStream(function () {
+        scanForDevices();
+    });
 });
